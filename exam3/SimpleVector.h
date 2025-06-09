a20596235f2050710a60c2dafd4783ce1689dfcb
anyway3 <tangle2002@naver.com>
2025-06-09 16:03:51 +0900

    도전 과제 추가 및 검수 완료

--- exam3/SimpleVector.h
+++ exam3/SimpleVector.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <iostream>
+#include <algorithm>
+
 template <typename T>
 class SimpleVector
 {
@@ -44,6 +46,37 @@ public:
 	}
 	int getSize() const {
 		return count; // 현재 크기 반환
+	}
+
+	void sort() {
+		std::sort(arr, arr + count); // 배열 정렬
+	}
+
+	void resize(int newSize) {
+		if (newSize < count) {
+
+
+			T* newarr = new T[newSize];
+			size = newSize; // 새로운 크기로 변경
+
+
+			for(int i = 0 ; i<newSize; i++) {
+				newarr[i] = arr[i]; // 기존 배열의 값 복사
+			}
+			delete[] arr;
+			arr = newarr;
+			count = newSize; // 크기를 줄임
+
+		}
+		else if (newSize > count) {
+			T* newarr = new T[newSize];
+			size = newSize; // 새로운 크기로 변경
+			for (int i = 0; i < newSize; i++) {
+				newarr[i] = arr[i]; // 기존 배열의 값 복사
+			}
+			delete[] arr;
+			arr = newarr;
+		}
 	}
 };
 
--- exam3/exam3.cpp
+++ exam3/exam3.cpp
@@ -5,25 +5,30 @@
 
 
 
-
 int main() {
 	SimpleVector<int> vec; // int형 벡터 생성
-	vec.push_back(10); // 10 추가
-	vec.push_back(20); // 20 추가
-	vec.push_back(30); // 30 추가
-	vec.push_back(40); // 40 추가
-	vec.push_back(50); // 50 추가
-	vec.push_back(60); // 60 추가
-	vec.push_back(70); // 70 추가
-	vec.push_back(80); // 80 추가
-	vec.push_back(90); // 90 추가
-	vec.push_back(100); // 100 추가
-	vec.push_back(110); // 110 추가 (크기 증가)
-	vec.push_back(120); // 120 추가 (크기 증가)
+	vec.push_back(10); 
+	vec.push_back(20); 
+	vec.push_back(30); 
+	vec.push_back(40);
+	vec.push_back(500); 
+	vec.push_back(600); 
+	vec.push_back(705);
+	vec.push_back(80); 
+	vec.push_back(90); 
+	vec.push_back(100); 
+	vec.push_back(110);
+	vec.push_back(120); 
 	std::cout << "전체 배열 출력";
 	for(int i = 0; i < vec.count; i++) {
 		std::cout << vec.arr[i] << " "; // 배열의 모든 요소 출력
 	}
+	vec.sort(); // 배열 정렬
+	std::cout << "\n정렬 후 배열 출력: ";
+	for (int i = 0; i < vec.count; i++) {
+		std::cout << vec.arr[i] << " "; // 배열의 모든 요소 출력
+	}
+
 	std::cout << std::endl;
 	std::cout <<"사용중인 배열 크기: " << vec.getSize()<<"\n배열 총 크기: "<<vec.size<<std::endl; // 현재 크기와 용량 출력
 std::cout<<"배열 제거"<< std::endl;
@@ -42,4 +47,6 @@ std::cout<<"배열 제거"<< std::endl;
 	for (int i = 0; i < 8; i++) {
 		std::cout << vec.pop_back() << " "; // 마지막 요소 제거 및 출력
 	}
+	vec.resize(5); // 배열 크기를 5로 조정
+	std::cout << "\n배열 크기 조정 후 사용중인 배열 크기: " << vec.getSize() << "\n배열 총 크기: " << vec.size << std::endl; // 현재 크기와 용량 출력
 }

3126f886c07c9e9dfd1018c91be610bf5e696ccf
anyway3 <tangle2002@naver.com>
2025-06-09 15:27:40 +0900

    초기 커밋.

---
+++ exam3/SimpleVector.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <iostream>
+template <typename T>
+class SimpleVector
+{
+
+public:
+	T* arr; // 10개의 요소를 가지는 배열
+	int size;
+	int count = 0;
+
+
+public:
+	SimpleVector() : size(10) {
+		arr = new T[size];
+	}
+
+	void push_back(T value) {
+		if (count < size) {
+			arr[count] = value; // 배열에 값 추가
+			count++; // 크기 증가
+		}
+		else {
+			T* newarr = new T[size + 5];
+			size = size + 5;
+			for (int i = 0; i < count; i++) {
+				newarr[i] = arr[i]; // 기존 배열의 값 복사
+			}
+			delete[] arr;
+			arr = newarr;
+			arr[count] = value;
+			count++;
+		}
+	}
+	T pop_back() {
+		if (count > 0) {
+			count--; // 크기 감소
+			return arr[count]; // 마지막 요소 반환
+		}
+		else {
+			std::cout << "배열이 비어있습니다." << std::endl;
+			return T(); // 기본값 반환
+		}
+	}
+};
+
+
---
+++ exam3/exam3.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+#include "SimpleVector.h"
+
+
+
+
+
+int main() {
+	SimpleVector<int> vec; // int형 벡터 생성
+	vec.push_back(10); // 10 추가
+	vec.push_back(20); // 20 추가
+	vec.push_back(30); // 30 추가
+	vec.push_back(40); // 40 추가
+	vec.push_back(50); // 50 추가
+	vec.push_back(60); // 60 추가
+	vec.push_back(70); // 70 추가
+	vec.push_back(80); // 80 추가
+	vec.push_back(90); // 90 추가
+	vec.push_back(100); // 100 추가
+	vec.push_back(110); // 110 추가 (크기 증가)
+	vec.push_back(120); // 120 추가 (크기 증가)
+	std::cout << "전체 배열 출력";
+	for(int i = 0; i < vec.count; i++) {
+		std::cout << vec.arr[i] << " "; // 배열의 모든 요소 출력
+	}
+std::cout<<"배열 제거"<< std::endl;
+	for(int i = 0; i < 5; i++) {
+		std::cout << vec.pop_back() << " "; // 마지막 요소 제거 및 출력
+	}
+	std::cout << std::endl;
+	std::cout << "제거 후 배열 출력: ";
+	for(int i = 0; i < vec.count; i++) {
+		std::cout << vec.arr[i] << " "; // 제거 후 배열의 모든 요소 출력
+	}
+}
